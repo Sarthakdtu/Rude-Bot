8e6f4f0f395d3089c585af2df30f35d282fb94e7
Sarthak <sarthaksrivastavadtu712@gmail.com>
2019-06-27 01:13:00 +0530

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rudebot.wsgi --log-file -
--- rudebot/settings.py
+++ rudebot/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
