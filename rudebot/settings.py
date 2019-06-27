bfe8a512435921078bbc9c21e572d3a65273ca4c
Sarthak <sarthaksrivastavadtu712@gmail.com>
2019-06-27 11:16:58 +0530

    all set

--- rudebot/settings.py
+++ rudebot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,8 +124,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
-django_heroku.settings(locals())
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
